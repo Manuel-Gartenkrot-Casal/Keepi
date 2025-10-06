9bba6448ec2f3a0a64bc09dfc2033c63a1fb39f0
Max <95097703@est.ort.edu.ar>
2025-10-06 09:02:08 -0300

    Primera interpretación de el proyecto incluyendo la primera base de todo
    6/10/25, primer día, primer visual

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KeepiProg.Models;
+
+namespace KeepiProg.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
