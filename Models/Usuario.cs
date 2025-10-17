41b5aaa650a7ee4d0ce643ffdc030242cd60a243
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-10-17 14:47:20 -0300

    Update BD junto a mínimos cambios en los model

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,11 +1,21 @@
-class Usuario{
-    public int ID {get;set;}
-    public string Username {get;set;}
-    public string Password {get;set;}
-    public Usuario (int ID, string Username, string Password)
+class Usuario
+{
+    public int ID { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+
+    public string Email { get; set; }
+
+    public Usuario(int ID, string Username, string Password, string Nombre, string Apellido, string Email)
     {
         this.ID = ID;
         this.Username = Username;
         this.Password = Password;
+        this.Nombre = Password;
+        this.Apellido = Password;
+        this.Email = Password;
+
     }
 }
