147fce94a26e72128f94bf2f89edb0fa1f4d4a4b
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-11-05 08:33:25 -0300

    Creada carpeta heladera en views, ahora te permite ingresar a MiHeladera

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9c00104c7fd228d5a9e3a8626f9bd0bfa13367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e710529abc752adc070936738355b837a3c1ff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
