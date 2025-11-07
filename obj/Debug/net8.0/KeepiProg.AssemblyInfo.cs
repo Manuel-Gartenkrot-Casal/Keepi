fdde40a5dd416242a8070cf14b70bc6caab97744
JUANA LOPEZ VALENZUELA <49002406@ort>
2025-11-07 08:26:13 -0300

    arreglar el header

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02436ac827c7664993b845ea5bcf228df141e25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aee53267d5a1dd52f0ad79e102f98dd09e490f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
