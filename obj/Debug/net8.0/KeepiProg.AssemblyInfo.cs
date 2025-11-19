039fccb5ba6e0f0fa45369a07a720fb575044168
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-11-19 08:48:29 -0300

    Fix link a MiHeladera en el menu lateral

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d4d2aa41306736e8b03409ea1599febeb0ba40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47462f645278dadcc1dfc5b2bfbab373b4952940")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
