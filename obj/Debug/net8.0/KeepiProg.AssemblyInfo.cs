e5f507375a749b1e087b8b13a8d365eae8db3687
MANUEL GARTENKROT CASAL <48792944@ort>
2025-11-19 13:29:34 -0300

    arregle el boton agregarProducto

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181c0352fa4de3577c0c4ad237b7a5d3cebd3223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b09b6ea433cb3f64c3acad1784143b0f476e21f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
