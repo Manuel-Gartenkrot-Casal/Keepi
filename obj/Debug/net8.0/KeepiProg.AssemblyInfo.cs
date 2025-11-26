e5a82e7e69c1b1eb8101b186ccbdc33536eef676
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-11-26 08:31:00 -0300

    Mejoras diseño ChatBot

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89bd32a714dc695cc7752e1f1cdd116be4b6fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff24ad5e10ebf84073b71283298bec38011e8194")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
