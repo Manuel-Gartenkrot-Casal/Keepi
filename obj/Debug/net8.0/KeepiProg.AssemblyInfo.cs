98654319edb001414bf2bb7f265c36032dbc69d1
AARON MARTIN KU <49194194@ort>
2025-11-28 15:58:00 -0300

    mejorando un poco de la landing

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304e85d0f3fd406e477c6ced591e726d060ede32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27db97c62fbbdffcbad1ee7072f02f77f0cac53f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
