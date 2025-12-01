e4622c2e5fca2fac3fbeb301896bc680c7451f51
MANUEL GARTENKROT CASAL <48792944@ort>
2025-12-01 08:54:41 -0300

    fix complilacion

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425f9b9aaa8ed74793882305fc6ad8eb9448e58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce7894dacea0beb6122ede864d4171b74b1c2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
