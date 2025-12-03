27e5d0e24d8e6539d6b5628f76808674d681246a
AARON MARTIN KU <49194194@ort>
2025-12-03 08:53:44 -0300

    mejorando el chatbot

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e7653c596c8d2c85ebc610ddde05f58cad10ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11df9a37709ee8b66406749089b3ca602f541a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
