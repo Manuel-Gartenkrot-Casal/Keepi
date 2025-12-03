c002759552967e7df399f35bcf62776466c2cc2c
Manuel-Gartenkrot-Casal <manucasal07@gmail.com>
2025-12-02 23:14:18 -0300

    agrego heladerita animada a login

--- obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
+++ obj/Debug/net8.0/KeepiProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce7894dacea0beb6122ede864d4171b74b1c2dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cda972d54fcbf4f64141a2f111c68ea9e762bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeepiProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
